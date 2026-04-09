751b7ead67cf6b737498b4dff775c7bc74e93fb3
Anshu-WebD <anshuweb@bigwigmediadigital.com>
2026-04-09 11:45:31 +0530

    updated storege

--- config/storage.js
+++ config/storage.js
@@ -5,7 +5,7 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "blogs", // Optional folder name
-    allowed_formats: ["jpg", "png", "jpeg"],
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
   },
 });
 
